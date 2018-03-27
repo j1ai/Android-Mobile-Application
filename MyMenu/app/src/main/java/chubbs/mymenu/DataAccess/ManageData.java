bdfd58fa99cda232fabd6296dc38928c46e4b289
j1ai <laijustinholam@gmail.com>
2018-03-27 06:27:25 -0400

    Add get documents

--- MyMenu/app/src/main/java/chubbs/mymenu/DataAccess/ManageData.java
+++ MyMenu/app/src/main/java/chubbs/mymenu/DataAccess/ManageData.java
@@ -1,6 +1,15 @@
 package chubbs.mymenu.DataAccess;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
@@ -52,6 +61,45 @@ public class ManageData extends BaseActivity{
         Map<String, Assessment> data = newAssessment.toMap();
         mFirestore.collection(uid).document("ASSESSMENTS")
                 .set(data, SetOptions.merge());
+    }
+
+    public void getDocument(String doc) {
+        // [START get_document]
+        DocumentReference docRef = mFirestore.collection(uid).document(doc);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null && document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        // [END get_document]
+    }
+
+    public void getAllDocs() {
+        // [START get_multiple_all]
+        mFirestore.collection(uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        // [END get_multiple_all]
     }
 
 }
