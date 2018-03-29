b708ce12872164e87957156830381500916e7728
j1ai <laijustinholam@gmail.com>
2018-03-29 03:55:17 -0400

    Add update delete methods for course and assessment

--- MyMenu/app/src/main/java/chubbs/mymenu/DataAccess/ManageData.java
+++ MyMenu/app/src/main/java/chubbs/mymenu/DataAccess/ManageData.java
@@ -10,6 +10,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -75,45 +76,93 @@ public class ManageData extends BaseActivity{
                 .set(data, SetOptions.merge());
     }
 
-    public void deleteDocument() {
-        // [START delete_document]
-        mFirestore.collection(uid).document("DC")
-                .delete()
+    // Delete a data instance of a Course/ Assessment Object
+    public void DeleteField(String doc, String name) {
+        // [START update_delete_field]
+        DocumentReference docRef = mFirestore.collection(uid).document(doc);
+
+        // Remove the 'capital' field from the document
+        Map<String,Object> updates = new HashMap<>();
+        updates.put(name, FieldValue.delete());
+
+        docRef.update(updates).addOnCompleteListener(new OnCompleteListener<Void>() {
+            // [START_EXCLUDE]
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {}
+            // [START_EXCLUDE]
+        });
+        // [END update_delete_field]
+    }
+
+    public void updateCourse(Course upCourse) {
+        // [START update_document]
+        DocumentReference courseRef = mFirestore.collection(uid).document("COURSES");
+        courseRef
+                .update("name", upCourse.getCid())
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                        Log.d(TAG, "DocumentSnapshot successfully updated!");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error deleting document", e);
+                        Log.w(TAG, "Error updating document", e);
                     }
                 });
-        // [END delete_document]
+        // [END update_document]
     }
 
     // Return the Specified Objects
     // For example, return CSC301 course in document ("COURSES")
-    public void getDocument(String doc,String name) {
+    public Course getCourse(final String name) {
         // [START get_document]
-        DocumentReference docRef = mFirestore.collection(uid).document(doc);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document != null && document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                    } else {
-                        Log.d(TAG, "No such document");
+        final List<Course> courseContainer = new ArrayList<>();
+        DocumentReference docRef = mFirestore.collection(uid).document("COURSES");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Map<String, Object> forms = documentSnapshot.getData();
+                for (Map.Entry<String, Object> form: forms.entrySet()) {
+                    String key = form.getKey();
+                    if (key.equals(name)){
+                        Map<String, Object> values = (Map<String, Object>)form.getValue();
+                        String cid = values.get("cid").toString();
+                        Course newCourse = new Course(cid);
+                        courseContainer.add(newCourse);
                     }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
                 }
             }
         });
         // [END get_document]
+        return courseContainer.get(0);
+    }
+
+    public Assessment getAssessment(final String name) {
+        // [START get_document]
+        final List<Assessment> assessmentContainer = new ArrayList<>();
+        DocumentReference docRef = mFirestore.collection(uid).document("ASSESSMENTS");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Map<String, Object> forms = documentSnapshot.getData();
+                for (Map.Entry<String, Object> form: forms.entrySet()) {
+                    String key = form.getKey();
+                    if (key.equals(name)){
+                        Map<String, Object> values = (Map<String, Object>)form.getValue();
+                        String name = values.get("name").toString();
+                        String weight = values.get("weight").toString();
+                        int w = Integer.parseInt(weight);
+                        String deadline = values.get("deadline").toString();
+                        Assessment assessment = new Assessment(name,w,deadline);
+                        assessmentContainer.add(assessment);
+                    }
+                }
+            }
+        });
+        // [END get_document]
+        return assessmentContainer.get(0);
     }
 
 
