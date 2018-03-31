9174d65c5e0ea965ca85aa5cbcf31e9a20f271a4
maj48 <jelderian@hotmail.com>
2018-03-31 19:04:06 -0400

    added some changes to viewsched, still not complete added screens for pat's part

--- MyMenu/app/src/main/java/chubbs/mymenu/ViewScheduleActivity.java
+++ MyMenu/app/src/main/java/chubbs/mymenu/ViewScheduleActivity.java
@@ -16,9 +16,13 @@ import java.util.ArrayList;
 import chubbs.mymenu.models.Course;
 import chubbs.mymenu.DataAccess.ManageData;
 import chubbs.mymenu.models.Assessment;
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-
-
+import java.util.concurrent.atomic.AtomicReference;
+import chubbs.mymenu.models.Job;
+import chubbs.mymenu.WeightedJob;
 public class ViewScheduleActivity extends MainActivity {
 
 
@@ -40,7 +44,7 @@ public class ViewScheduleActivity extends MainActivity {
 
 
 
-        ListView list;
+        final ListView list;
         //Set Up managing data
 
         adapter=new ArrayAdapter<>(getApplicationContext(),
@@ -52,13 +56,49 @@ public class ViewScheduleActivity extends MainActivity {
 
 
 
-        Button rmElement = findViewById(R.id.rmElement);
-        rmElement.setOnClickListener(new OnClickListener(){
+        Button addElement = findViewById(R.id.addElement);
+        addElement.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
 
 
+                datePicker = (DatePicker) findViewById(R.id.datePicker);
+                day = datePicker.getDayOfMonth();
+                month = datePicker.getMonth() + 1; //indexes start at 0
+                year = datePicker.getYear();
 
 
+
+
+
+                String date = day + "/" + month + "/" + year;
+                List<Assessment> assessments = db.getAll_assessment();
+                Assessment [] listOfAssess = new Assessment[assessments.size()];
+                listOfAssess = assessments.toArray(listOfAssess);
+                Job[] jobs = WeightedJob.convert(listOfAssess);
+                ArrayList<Job> schedule = WeightedJob.schedule(jobs);
+
+                listItems.clear();
+//                listItems.add("Potato");
+//
+////                listItems.add(course +"     "+ name + "     "
+////                        + w +
+////                        "%     " +
+////                        date);
+//                listItems.add(String.valueOf(jobs.length));
+//                listItems.add(String.valueOf(schedule.size()));
+//
+//                for (Job j : jobs){
+//                    listItems.add(j.getStartTime()+":"+j.getFinishTime()+":"+j.getName());
+//                    //listItems.add(String.valueOf(jobs.length));
+//                }
+
+
+//                Assessment newAssessment = new Assessment(course,name,weightvalue,date);
+//                db.addAssessment(newAssessment);
+                adapter.notifyDataSetChanged();
+
+
+                db.getAllAssessments();
 
             }
         });
